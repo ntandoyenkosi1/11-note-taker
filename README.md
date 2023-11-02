d616b7437629d66783a397639f6a5909f5c3800c
Ntandoyenkosi <nmendu@2u.com>
2023-11-02 10:10:13 +0200

    Fixed minor issues and added delete functionality Updated README file with a screenshot and updated URL

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Note Taker
+This is a note taking application.
+Developed using NodeJS and HTML and CSS.
 
+![Note taker](./public/assets/chrome_xKMP8f4bp6.png)
 ## License
 MIT
 ## Production
-Deployed at: https://infinite-lowlands-17106.herokuapp.com/notes
+Deployed as a service at: https://note-taker-cck37mrz7a-ue.a.run.app/
--- api/notes.js
+++ api/notes.js
@@ -1,13 +1,22 @@
 const fs = require("fs")
 const path = require("path")
 const router = require("express").Router()
-var data = require("../db/db.json")
+
 router.get("/notes", (req, res) => {
+  let data = fs.readFileSync(path.join(process.cwd()+"/db/db.json"), {encoding:"utf8"})
   res.send(data)
 })
 router.post("/notes", (req, res) => {
+  let data = require("../db/db.json")
   data = data.sort(function (a, b) { return a.id - b.id })
   data.push({ id: data.length, title: req.body.title, text: req.body.text })
+  fs.writeFileSync(path.join(process.cwd(), "db/db.json"), JSON.stringify(data))
+  res.sendStatus(200)
+})
+router.delete("/notes/:id", (req, res) => {
+  const {id}=req.params
+  let data = require("../db/db.json")
+  data = data.filter(x=>x.id!=id)
   fs.writeFileSync(path.join(process.cwd(), "db/db.json"), JSON.stringify(data))
   res.sendStatus(200)
 })
